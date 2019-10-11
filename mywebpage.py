82d7750300a5bf87d5dcd2d96a1c8cf8260eb325
jocelyngallardo <54326467+jocelyngallardo@users.noreply.github.com>
2019-10-11 09:42:15 -0700

    Update mywebpage.py

--- mywebpage.py
+++ mywebpage.py
@@ -2,17 +2,17 @@ from flask import Flask, url_for, render_template
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-@app.route("/")
-def render_main():
+@app.route("/colors")
+def render_colors():
     return render_template('colors.html')
 
-@app.route("/p1")
+@app.route("/money")
 def render_page1():
     return render_template('money.html')
 
-@app.route("/p2")
-def render_page2():
+@app.route("/measurements")
+def render_measurements():
     return render_template('measurements.html')
     
-if __name__=="__main__":
+if __name__=="__colors__":
     app.run(debug=False)
