3f387d8244799034d161a16091847ddb984ad507
jocelyngallardo <54326467+jocelyngallardo@users.noreply.github.com>
2019-10-17 09:31:30 -0700

    Update mywebpage.py

--- mywebpage.py
+++ mywebpage.py
@@ -3,8 +3,13 @@ from flask import Flask, url_for, render_template
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
-def render_colors():
+def render_main():
     return render_template('colors.html')
+
+@app.route("/colors")
+def render_colors():
+    response = returnColor
+    return render_template('response.html', responseFromServer = response)
 
 @app.route("/money")
 def render_money():
@@ -16,3 +21,17 @@ def render_measurements():
     
 if __name__=="__colors__":
     app.run(debug=False)
+
+def returnColor(english, spanish)
+    if english = "red"
+        return spanish = "rojo"
+    if english = "orange"
+        return spansih = "naranja"
+    if english = "yellow"
+        return = "amarillo"
+    if english = "green"
+        return spanish = "verde"
+    if english = "blue"
+        return spanish = "azul"
+    if english = "purple"
+        return spanish = "morado"
