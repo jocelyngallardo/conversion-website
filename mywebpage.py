de4030a6cc78ae0c49fee3b839babf67dad260ad
jocelyngallardo <54326467+jocelyngallardo@users.noreply.github.com>
2019-10-18 08:46:54 -0700

    Update mywebpage.py

--- mywebpage.py
+++ mywebpage.py
@@ -11,17 +11,18 @@ def render_response():
     response = returnColor(request.args['color'])
     return render_template('response.html', responseFromServer = response)
 
+@app.route("/measurements")
+def render_measurements():
+    if "inches" in request.args:
+        inches = float(request.args['inches']
+        response = str( inches * 2.54) + "centimeters"
+        return render_template('response.html', responseFromServer = response)
+    else:
+        return render_template('measurements.html')
+
 @app.route("/money")
 def render_money():
     return render_template('money.html')
-
-@app.route("/response")
-def render_response():
-    return render_template('response.html', responseFromServer = response)
-
-@app.route("/measurements")
-def render_measurements():
-    return render_template('measurements.html')
 
 @app.route("/response")
 def render_response():
